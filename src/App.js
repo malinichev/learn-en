7f20876c53733cd320cd62f9ec4dc0f8df3af990
malinichev_s <malinichev_s@mail.ru>
2022-03-16 13:36:33 +0800

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+  return <View>asdasd</View>
 }
 
-export default App;
+const View = styled.div`
+  color: red;
+`
