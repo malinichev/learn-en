71987c0ee1bcf403de3896cd83f4afdca3fa1c4b
malini4 <malinichev_s@mail.ru>
2022-03-19 13:09:38 +0800

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
+import React, { useState } from 'react'
 import styled from 'styled-components'
-
+import { Item } from './Item'
 export const App = () => {
-  return <View>asdasd</View>
+  const times = ['past', 'main', 'future']
+
+
+  return (
+    <View>
+      <Title>Учу Инглиш</Title>
+      <SubTitle>To Be</SubTitle>
+      <Content>
+          <Column>
+
+          {Array.from({length: 10}, (_, i) => i + 1).map((_,index)=><Item key={String(index)} time={times[getRandom(times)]} />)}
+          </Column>
+          <Column>
+
+          {Array.from({length: 10}, (_, i) => i + 1).map((_,index)=><Item key={String(index)} time={times[getRandom(times)]} />)}
+          </Column>
+      </Content>
+    </View>
+  )
+
+  function getRandom(arr) {
+    const random = Math.floor(Math.random() * arr.length)
+
+    return random
+  }
 }
 
 const View = styled.div`
-  color: red;
+  padding: 100px 100px 200px;
+`
+
+const Title = styled.div`
+  font-size: 14px;
+`
+const SubTitle = styled.div`
+  font-size: 12px;
+`
+const Content = styled.div`
+  display: flex;
+  margin-top: 50px;
+  justify-content: space-around;
+`
+const Column = styled.div`
+  display: flex;
+  flex-flow: column;
 `
---
+++ src/Item.js
@@ -0,0 +1,153 @@
+import React, { useState } from 'react'
+import styled, { css } from 'styled-components'
+
+const init_words = [
+	'here',
+	'away',
+	'near',
+	'at home',
+	'at school',
+	'on leave',
+	'polite',
+	'kind',
+	'there',
+	'back',
+	'far',
+	'at work',
+	'in Paris',
+	'on duty',
+	'rude',
+	'cruel',
+	'short',
+	'fat',
+	'old',
+	'poor',
+	'silly',
+	'ill',
+	'sad',
+	'lazy',
+	'tall',
+	'slim',
+	'young',
+	'rich',
+	'cleaver',
+	'healthy',
+	'merry',
+	'wise'
+]
+
+const middle_word = ['very', 'too', 'rather', 'somewhat', 'a bit', 'quite']
+
+const initData = [
+	{
+		name: ['I'],
+		rightAnswer: {
+			past: 'was',
+			main: 'am',
+			future: 'will be'
+		}
+	},
+	{
+		name: ['He', 'She', 'It'],
+		rightAnswer: {
+			past: 'was',
+			main: 'is',
+			future: 'will be'
+		}
+	},
+	{
+		name: ['We', 'You', 'They'],
+		rightAnswer: {
+			past: 'were',
+			main: 'are',
+			future: 'will be'
+		}
+	}
+]
+
+const selectData = {
+	past: ['was', 'were'],
+	main: ['am', 'is', 'are'],
+	future: ['will be']
+}
+
+const title = {
+	past: 'Прошедшее',
+	main: 'Настоящее',
+	future: 'Будующее'
+}
+
+export const Item = ({ time }) => {
+	const [sectionValue, setSectionValue] = useState('')
+
+	const [rndSection] = useState(initData[getRandom(initData)])
+	const [rndName] = useState(rndSection?.name[getRandom(rndSection?.name)])
+	const [middleWord] = useState(middle_word[getRandom(middle_word)])
+	const [endWord] = useState(init_words[getRandom(init_words)])
+	const [isError, setError] = useState(false)
+
+
+	return (
+		<View>
+			<ItemTitle>{title[time]}</ItemTitle>
+			<ItemContent isError={isError} isChanged={sectionValue !== ""}>
+				{rndName}{' '}{renderSelect(selectData[time])}{' '}{middleWord}{' '}
+				{endWord}
+			</ItemContent>
+		</View>
+	)
+
+	function handleChange(event) {
+		setError(rndSection?.rightAnswer[time] !== event.target.value)
+		setSectionValue(event.target.value)
+	}
+
+	function renderSelect(data) {
+		const options = ['', ...data]
+		return (
+			<Select onChange={handleChange} value={sectionValue}>
+				{data?.length > 0 &&
+					options.map((option, index) => (
+						<option key={String(index)} value={option}>
+							{option}
+						</option>
+					))}
+			</Select>
+		)
+	}
+
+	function getRandom(arr) {
+		return Math.floor(Math.random() * arr.length)
+	}
+}
+
+const View = styled.div`
+	display: flex;
+	flex-flow: column;
+	
+	&:not(:first-child){
+		margin-top: 10px;
+	}
+`
+const ItemTitle = styled.div`
+	font-size: 22px;
+	font-style: italic;
+	color: darkgrey;
+`
+const ItemContent = styled.div`
+ font-size: 24px;
+
+ ${({ isError, isChanged }) =>
+	isChanged &&
+	(isError
+		? css`
+     color: red;
+     `
+		: css`
+     color: green;
+     `)}
+`
+
+const Select = styled.select`
+ font-size: 14px;
+`
